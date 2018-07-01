a2aff944e4b4681099e6448c5c68b1f01f2b2b37
Guilherme GM <guilherme.moreira@beetech.global>
2018-06-30 23:56:11 -0300

    add SearchTree.js, swifts_list.txt

---
+++ SearchTree.js
@@ -0,0 +1,105 @@
+const fs = require("fs");
+
+const readFile = (fileName, type) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(
+      fileName,
+      type,
+      (err, data) => (err ? reject(err) : resolve(data))
+    );
+  });
+
+class SearchTree {
+  constructor() {
+    this.tree = { 0: {} };
+  }
+
+  addWord({ word }) {
+    return this.processWord({
+      word: new String(word.toLowerCase().trim())
+    });
+  }
+
+  processWord({ word, index = 0, level = 0, node = this.tree[level] }) {
+    if (index === word.length) {
+      if (level === word.length - 1) {
+        return;
+      }
+
+      level += 1;
+
+      if (!this.tree[level]) {
+        this.tree[level] = {};
+      }
+
+      return this.processWord({
+        word,
+        level,
+        index: level,
+        node: this.tree[level]
+      });
+    }
+
+    const letter = word[index];
+
+    if (!node[letter]) {
+      node[letter] = {
+        words: []
+      };
+    }
+
+    node[letter]["words"].push(word);
+
+    return this.processWord({
+      word,
+      level,
+      index: ++index,
+      node: node[letter]
+    });
+  }
+
+  search({ query, index = 0, level = 0, node = this.tree[level] }) {
+    const letter = query[index];
+
+    if (!letter) {
+      return node.words;
+    }
+
+    if (node[letter]) {
+      return this.search({
+        query,
+        index: ++index,
+        level,
+        node: node[letter]
+      });
+    }
+
+    level++;
+
+    return this.search({
+      query,
+      index: 0,
+      level,
+      node: this.tree[level]
+    });
+  }
+}
+
+const main = async () => {
+  const tree = new SearchTree();
+  
+  const wordsList = await readFile("./swifts_list.txt", "utf8");
+  words = wordsList.trim().split("\n");
+
+  words.forEach(word => {
+    tree.addWord({ word });
+  });
+
+  try {
+    console.log(tree.search({ query: "z22" }));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+main();
